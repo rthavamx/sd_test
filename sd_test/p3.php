b79f024c7f5bde6920f7e7ca6ed90eee5a0243af
Thavamani <ravishankarx.thavamani@intel.com>
2017-07-07 15:59:47 +0530

    again push

--- sd_test/p3.php
+++ sd_test/p3.php
@@ -2,3 +2,11 @@
 $cars = array("Volvo","BMW","Toyota");
 print "$cars[2]";
 ?>
+
+<?php
+$count = flag_get_user_flag_counts(flag_get_flag('product_comparison'), $user);
+if ($count >4){
+    echo 'Compare limit achieved';
+}
+//
+?>
