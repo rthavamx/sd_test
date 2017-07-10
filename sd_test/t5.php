1803c0779a13fbdddd16fdd6f5074fab6dd5b3f6
Thavamani <ravishankarx.thavamani@intel.com>
2017-07-10 16:16:17 +0530

    push for US

---
+++ sd_test/t5.php
@@ -0,0 +1,31 @@
+<?php
+$x = 5;
+$y =2;
+function myTest() {
+	$y =20;
+    //global $y; => 
+	$y =3;
+    echo "<p>Variable x  function is: $y</p>";
+} 
+function myTest3() {
+     $x = 0;
+    echo $x;
+    $x++;
+}
+function myTest2() {
+	   $y =20;
+	   global $x;
+	   $y = $x+$y;
+    echo "<p>Variable x  function is: $x</p>";
+	echo "<p>Variable y  function is: $y</p>";
+	print "$x";
+	print "$x";
+}
+myTest3();
+myTest3();
+myTest3();
+myTest2();
+myTest();
+echo "<p>Variable x  function is: $x</p>";
+echo "<p>Variable y  function is: $y</p>";
+?>
